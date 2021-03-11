e448dc5c6d2ba90a03a86187c02c4fdd2418b34a
Eneias Ramos de Melo <eneias@gamuza.com.br>
2021-03-10 23:28:10 -0300

    Fix undefined symbol on Solaris: mono_w32file_get_file_system_type

--- mono/metadata/w32file-unix.c
+++ mono/metadata/w32file-unix.c
@@ -4737,7 +4737,7 @@ mono_w32file_get_drive_type(const gunichar2 *root_path_name)
 	return (drive_type);
 }
 
-#if defined (HOST_DARWIN) || defined (__linux__) || defined(HOST_BSD) || defined(__FreeBSD_kernel__) || defined(__HAIKU__) || defined(_AIX)
+#if defined (HOST_DARWIN) || defined (__linux__) || defined(HOST_BSD) || defined(__FreeBSD_kernel__) || defined(__HAIKU__) || defined(_AIX) || (defined(__sun__) && defined(__svr4__))
 static gchar*
 get_fstypename (gchar *utfpath)
 {
